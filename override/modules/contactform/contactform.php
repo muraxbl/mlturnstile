ae65a8e63cf504f3ec4cff613a730cc700cb2589
muraxbl <72076280+muraxbl@users.noreply.github.com>
2026-03-30 17:41:52 +0200

    Refactor sendMessage method in ContactformOverride

--- override/modules/contactform/contactform.php
+++ override/modules/contactform/contactform.php
@@ -3,15 +3,10 @@ class ContactformOverride extends Contactform
 {
     public function sendMessage()
     {
-        // Obtener instancia del módulo
         $module = Module::getInstanceByName('mlturnstile');
-
         if ($module && $module->active) {
-            // Ejecutar SOLO tu validación
             $module->hookActionContactFormSubmitBefore([]);
         }
-
-        // Si hay errores → NO enviar
         if (!count($this->context->controller->errors)) {
             return parent::sendMessage();
         }
